ee716d900ab38dbda4fcee7d4453532e02dfa377
Eoghan-Monahan <47867908+Eoghan-Monahan@users.noreply.github.com>
2019-02-21 22:48:24 +0000

    Initial Commit

---
+++ sketch_feb19a.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(31250);
+  int i;
+  for (i = 2; i < 14; i++) {
+    pinMode(i, INPUT);
+  }
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  int j = 0;
+  String inputs = "";
+  for (i = 2; i < 14; i++) {
+    inputs += (char) digitalRead(i);
+  }
+  inputs += "\n";
+  Serial.print(inputs);
+  delay(20);
+
+}
