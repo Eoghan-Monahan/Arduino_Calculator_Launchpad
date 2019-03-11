04389550320f21132b1645ab1e328f02c6bb57fb
Eoghan-Monahan <47867908+Eoghan-Monahan@users.noreply.github.com>
2019-03-11 10:03:35 +0000

    Updating Test Code
    Updating test code to match with change in project direction

--- sketch_feb19a/sketch_feb19a.ino
+++ sketch_feb19a/sketch_feb19a.ino
@@ -1,23 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(31250);
-  int i;
-  for (i = 2; i < 14; i++) {
-    pinMode(i, INPUT);
-  }
+/*4x4 Matrix Keypad connected to Arduino
+This code prints the key pressed on the keypad to the serial port*/
 
+#include <Keypad.h>
+
+const byte numRows= 4; //number of rows on the keypad
+const byte numCols= 4; //number of columns on the keypad
+
+//keymap defines the key pressed according to the row and columns just as appears on the keypad
+char keymap[numRows][numCols]=
+{
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {9,8,7,6}; //Rows 0 to 3
+byte colPins[numCols]= {5,4,3,2}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+void setup()
+{
+Serial.begin(38400);
 }
-void loop() {
-  // put your main code here, to run repeatedly:
-  int i;
-  int j = 0;
-  String inputs = "";
-  //reads in all inputs
-  for (i = 2; i < 14; i++) {
-    inputs += (char) digitalRead(i);
-  }
-  inputs += "\n";
-  Serial.print(inputs);
-  delay(20);
 
+//If key is pressed, this key is stored in 'keypressed' variable
+//If key is not equal to 'NO_KEY', then this key is printed out
+//if count=17, then count is reset back to 0 (this means no key is pressed during the whole keypad scan process
+void loop()
+{
+for (int i=0; i<9; i++) {
+char keypressed = myKeypad.getKey();
+if (keypressed != NO_KEY)
+{
+Serial.println(keypressed);
+}
+}
 }
